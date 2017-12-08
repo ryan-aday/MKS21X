8f85b1e0bcfef58368830cb1e468394e00ba9990
ryan-aday <raday@stuy.edu>
2017-12-08 05:23:43 -0500

    insertionSOrt & selcetionSort both function

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -42,26 +42,27 @@ public class Sorts{
     public static void selectionSort(int[] data){
 	int min;
 	
-	for (int count=0; count<data.length; count++){
-	    min= data[count];
-	    for (int counts=1; counts<data.length; counts++){
-		if (data[counts]<min){
-		    swap(data, count, counts);
-		    count=0;
-		    min=data[count];
+	for (int count=0; count<data.length-1; count++){
+	    min= count;
+	    for (int counts=count+1; counts<data.length; counts++){
+		if (data[counts]<data[min]){
+		    min=counts;
 		}
-	    }
+	    }swap(data, count, min);
 	}
     }
 
     public static void insertionSort(int[] data){
-	for (int count=1; count < data.length-1; count++){
-	    if (data[count]>data[count+1]){
-		swap(data, count, count+1);
-		    }
+	for (int count=1; count < data.length; count++){
+	    int min=data[count];
+	    int comp=count-1;
+	    while(comp >=0  && data[comp]<min){
+		data[comp+1]=data[comp];
+		comp--;
+	    }
+	    data[comp+1]=min;
 	}
     }
-	    
     
     public static void main(String[]artie){                                    
         int[] randish = new int[5];                                            
@@ -71,9 +72,9 @@ public class Sorts{
 	System.out.println(Arrays.toString(randish));
 	swap(randish, 0, 1);
         System.out.println(Arrays.toString(randish));                           
-        //      bogoSort(randish);
+             bogoSort(randish);
 	System.out.println(Arrays.toString(randish));                         
-        insertionSort(randish);
+        //insertionSort(randish);
         System.out.println(Arrays.toString(randish));                         
 	//	selectionSort(randish);
 	System.out.println(Arrays.toString(randish));    
